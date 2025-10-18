1377535dc84669a744d58f84d1932abadc2b1ed9
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2025-10-18 20:10:34 +0530

    updated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,8 +19,8 @@ const Login = () => {
     e.preventDefault();
     setLoading(true);
 
-    try {
-      const res = await axios.post("http://localhost:5000/login", {
+       try {
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/login`, {
         email,
         password,
       });
